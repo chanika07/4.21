4a48c4705725cb71a3cd261f8dc62e727c25e3f6
chanika07 <chanika.ket@gmail.com>
2020-04-21 17:13:56 +0700

    แก้ให้หน่อยค้าบ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        int r = sc.nextInt();
-        int c = sc.nextInt();
-        int [][]a = new int[r+1][c+1];
-        int n = sc.nextInt();
-        for (int i=1; i<=n; i++){
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            if (a[x][y]==0){
-                System.out.println("reserved complete");
-                a[x][y]=1;
+        String tex = sc.nextLine();
+        for (int i=0;i<tex.length();i+=5){
+            if (i+5>tex.length()) {
+
+                System.out.println(tex.substring(i, tex.length()));
             }
-            else {
-                System.out.println("reserved not complete");
+            else{
+                System.out.println(tex.substring(i,i+5));
             }
+            int c=0;
+            for (int j=0 ;j<tex.length();j++){
+
+                if (tex.charAt(j)==97 ||tex.charAt(j)==101 ||
+                        tex.charAt(j)==105 ||
+                        tex.charAt(j)==111 ||tex.charAt(j)==117 ){
+                    c++;
+                }
+            }System.out.println(" "+c);
         }
+
+
     }
 }
